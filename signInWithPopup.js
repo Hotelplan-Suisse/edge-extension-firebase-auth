44520867b1c44c387e7cadf90782760d756501f7
Tim <16090191+tw-hotelplan@users.noreply.github.com>
2024-11-23 01:58:38 +0100

    Update signInWithPopup.js

--- signInWithPopup.js
+++ signInWithPopup.js
@@ -1,32 +1,51 @@
-import { signInWithPopup, GoogleAuthProvider, getAuth } from'firebase/auth';
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from './firebaseConfig.js'
+import { initializeApp } from "firebase/app";
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+import {
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithPopup
+} from "firebase/auth";
 
-// This code runs inside of an iframe in the extension's offscreen document.
-// This gives you a reference to the parent frame, i.e. the offscreen document.
-// You will need this to assign the targetOrigin for postMessage.
-const PARENT_FRAME = document.location.ancestorOrigins[0];
+const [targetOrigin] = document.location.ancestorOrigins;
 
-// This demo uses the Google auth provider, but any supported provider works.
-// Make sure that you enable any provider you want to use in the Firebase Console.
-// https://console.firebase.google.com/project/_/authentication/providers
-const PROVIDER = new GoogleAuthProvider();
+console.log("SignInProxy's ancestors: ", document.location.ancestorOrigins);
 
-function sendResponse(result) {
-  globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
-}
+const app = initializeApp({
+    apiKey: "AIzaSyCfEHY2kSz_aB8tCSCtDJMzr5MPDjs-_kE",
+    authDomain: "hotelplan-suisse.github.io",
+});
 
-globalThis.addEventListener('message', function({data}) {
-  if (data.initAuth) {
-    // Opens the Google sign-in page in a popup, inside of an iframe in the
-    // extension's offscreen document.
-    // To centralize logic, all respones are forwarded to the parent frame,
-    // which goes on to forward them to the extension's service worker.
-    signInWithPopup(auth, PROVIDER)
-      .then(sendResponse)
-      .catch(sendResponse)
-  }
+const auth = getAuth(app);
+auth.useDeviceLanguage();
+
+const sendResponse = result => {
+    // resolved: result is UserCredential
+    // https://github.com/firebase/firebase-js-sdk/blob/ffbf5a60ac756c69dd50bddb69fccd9968844ac5/packages/auth/src/model/public_types.ts#L1058
+    
+    // rejected: result is FirebaseError
+    // https://github.com/firebase/firebase-js-sdk/blob/main/packages/auth/src/core/util/assert.ts
+    // https://github.com/firebase/firebase-js-sdk/blob/main/packages/util/src/errors.ts
+
+    globalThis.parent.self.postMessage(
+        JSON.stringify(result),
+        targetOrigin
+    );
+};
+
+globalThis.addEventListener("message", event => {
+    // TODO: check/validate/verify targetOrigin, event.origin, event.source
+
+    console.log("SignInProxy got a message: ", event);
+
+    const provider = new GoogleAuthProvider();
+
+    event.data?.initAuth && signInWithPopup(
+        auth,
+        provider
+    ).then(
+        sendResponse
+    ).catch(
+        sendResponse
+    )
 });
